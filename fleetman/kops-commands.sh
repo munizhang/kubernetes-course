3286bf20d5af9be8f7d09c0ba391ab351fe6ba27
Mubai Zhang <munimunizhang@gmail.com>
2020-04-22 14:43:01 -0400

    change storage to cloud ssd

--- fleetman/kops-commands.sh
+++ fleetman/kops-commands.sh
@@ -5,5 +5,8 @@ ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa
 kops create secret --name ${NAME} sshpublickey admin -i ~/.ssh/id_rsa.pub
 kops create cluster --zones us-east-2a,us-east-2b,us-east-2c ${NAME}
 kops edit cluster ${NAME}
-kops edit ig nodes --name ${NAME} ## ig - instance groups
-kops get ig nodes --name ${NAME} ## verify the instance groups
+# ig - instance groups, min nodes and max nodes, aws auto scaling group
+kops edit ig nodes --name ${NAME} 
+kops get ig --name ${NAME} ## verify the instance groups
+kops update cluster --name ${NAME}
+kops validate cluster
