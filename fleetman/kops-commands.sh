0ddbccd299339494978941dedf2218cd47d640ca
Mubai Zhang <munimunizhang@gmail.com>
2020-04-23 11:18:51 -0400

    add ELK stack with Fluentd

--- fleetman/kops-commands.sh
+++ fleetman/kops-commands.sh
@@ -1,9 +1,11 @@
-export AWS_SECRET_ACCESS_KEY= $(aws configure get aws_secret_access_key)
-export AWS_ACCESS_KEY= $(aws configure get aws_access_key)
+export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
+export AWS_ACCESS_KEY=$(aws configure get aws_access_key)
 aws ec2 describe-availability-zones --region us-east-2
 ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa
+NAME=fleetman.k8s.local
+KOPS_STATE_STORE=s3://kops-state-mubzhang
 kops create secret --name ${NAME} sshpublickey admin -i ~/.ssh/id_rsa.pub
-kops create cluster --zones us-east-2a,us-east-2b,us-east-2c ${NAME}
+kops create cluster --name ${NAME} --zones us-east-2a,us-east-2b,us-east-2c 
 kops edit cluster ${NAME}
 # ig - instance groups, min nodes and max nodes, aws auto scaling group
 kops edit ig nodes --name ${NAME} 
