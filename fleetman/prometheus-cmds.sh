1d47731b37a661e57232654ec0ddcadba1edd00c
Mubai Zhang <munimunizhang@gmail.com>
2020-04-24 11:43:13 -0400

    revert slack url

--- fleetman/prometheus-cmds.sh
+++ fleetman/prometheus-cmds.sh
@@ -1,3 +1,5 @@
 kubectl create ns monitoring
 helm install monitoring stable/prometheus-operator --namespace monitoring 
 kubectl config view --minify
+#alertmanager
+kubectl logs -n monitoring alertmanager-kube-prometheus-0
