e27952b6bdff64207d0211fca534a75a0d3102ba
Mubai Zhang <munimunizhang@gmail.com>
2020-11-28 09:44:53 -0500

    prometheus

--- fleetman/prometheus-cmds.sh
+++ fleetman/prometheus-cmds.sh
@@ -3,3 +3,4 @@ helm install monitoring stable/prometheus-operator --namespace monitoring
 kubectl config view --minify
 #alertmanager
 kubectl logs -n monitoring alertmanager-kube-prometheus-0
+ubectl create secret -n monitoring generic alertmanager-monitoring-prometheus-oper-alertmanager --from-file=alertmanager.yaml
